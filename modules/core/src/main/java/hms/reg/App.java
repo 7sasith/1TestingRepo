11a28c81466e9f4d6a5d24726868c0de78caca13
Sasith Imbuldeniya <7sasith@gmail.com>
2015-12-22 11:42:46 +0530

    code standed

--- modules/core/src/main/java/hms/reg/App.java
+++ modules/core/src/main/java/hms/reg/App.java
@@ -12,8 +12,7 @@ package hms.reg;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
